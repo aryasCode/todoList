fa01a09cdc9ba239f05250a4da43c5b43bdeef0b
Arya Srivastava <aryashriyasrivastava@gmail.com>
2020-07-14 13:59:08 -0700

    1st Version

---
+++ todoListApp/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  todoListApp
+//
+//  Created by Arya Srivastava on 7/14/20.
+//  Copyright © 2020 Arya Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+    
+}
